f8b8c9c5fc468194aef97d460ec33ed233129751
vesakonjufca <162922281+vesakonjufca@users.noreply.github.com>
2024-12-04 14:42:41 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("learn-more-btn").addEventListener("click", function() {
+    const moreInfo = document.getElementById("more-info");
+    if (moreInfo.style.display === "none") {
+        moreInfo.style.display = "block";
+    } else {
+        moreInfo.style.display = "none";
+    }
+});
